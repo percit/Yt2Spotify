e4aceaa3b514aecc5bfbcc608cdd4749b324597b
percit <marcinzdunek100@gmail.com>
2024-01-23 16:49:35 +0100

    Add deleting songs already added to spotify This unfortunetely won't work, as oauth 2.0 is needed to delete elements from yt playlist

--- main.go
+++ main.go
@@ -45,10 +45,11 @@ func main() {
 	}
 
 	var unwantedSongs []string
+	var songsAddedToSpotify []string
 	fmt.Println("\nChoose from 0 to 9 to add a song to the playlist, or enter 11 to skip the song:\n")
 	for _, song := range songs {
-		fmt.Println("Youtube Song:" + song)
-		results, err := client.Search(context.Background(), song, spotify.SearchTypeTrack)
+		fmt.Println("Youtube Song:" + song.Title)
+		results, err := client.Search(context.Background(), song.Title, spotify.SearchTypeTrack)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -73,20 +74,27 @@ func main() {
 		if helpers.IsNumberInRange(userReply) {
 			fmt.Println("You chose song:", results.Tracks.Tracks[userReply].Name, results.Tracks.Tracks[userReply].Artists[0].Name)
 			client.AddTracksToPlaylist(context.Background(), spotify.ID(spotifyPlaylist), results.Tracks.Tracks[userReply].ID)
+			songsAddedToSpotify = append(songsAddedToSpotify, song.Title)
+			yt.DeleteSongByID(ytPlaylistID, song.PlaylistItemID, helpers.GoogleApiToken)//TODO this should actually delete from yt playlist the ones that are on spotify
 		} else if userReply == 11 {
 			fmt.Println("You chose to skip this song")
-			unwantedSongs = append(unwantedSongs, song) //TODO this should actually delete from yt playlist the ones that are on spotify
+			unwantedSongs = append(unwantedSongs, song.Title)
 		} else {
 			fmt.Println("Something is wrong")
 		}
 		fmt.Println("\n")
 	}
 
-	content := strings.Join(unwantedSongs, "\n")
-	errFile := ioutil.WriteFile("song_list.txt", []byte(content), 0644)
+	contentUnwantedSongs := strings.Join(unwantedSongs, "\n")
+	errFile := ioutil.WriteFile("unwantedSongs.txt", []byte(contentUnwantedSongs), 0644)
+	if err != nil {
+		log.Fatalf("Unable to write to file: %v", errFile)
+	}
+	contentSongsAddedToSpotify := strings.Join(unwantedSongs, "\n")
+	errFile = ioutil.WriteFile("songsAddedToSpotify.txt", []byte(contentSongsAddedToSpotify), 0644)
 	if err != nil {
 		log.Fatalf("Unable to write to file: %v", errFile)
 	}
 
-	fmt.Println("Song list saved to 'song_list.txt'")
+	fmt.Println("Song list saved to 'unwantedSongs.txt' and 'songsAddedToSpotify.txt'")
 }
--- yt/yt.go
+++ yt/yt.go
@@ -2,33 +2,43 @@ package yt
 
 import (
 	"context"
+	"fmt"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
-func GetYoutubePlaylistItems(playlistID string, apiKey string) ([]string, error) {
+type PlaylistInfo struct {
+	Title          string
+	PlaylistItemID string
+}
+
+func GetYoutubePlaylistItems(playlistID string, apiKey string) ([]PlaylistInfo, error) {
 	ctx := context.Background()
 	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
 	if err != nil {
-		return nil, err
+		return nil,err
 	}
 
-	var playlistItems []string
+	var playlistItems []PlaylistInfo
 	nextPageToken := ""
 	for {
-		playlistCall := youtubeService.PlaylistItems.List([]string{"snippet"}).
+		playlistCall := youtubeService.PlaylistItems.List([]string{"snippet", "id"}).
 			PlaylistId(playlistID).
 			MaxResults(50).
 			PageToken(nextPageToken)
 
 		playlistResponse, err := playlistCall.Do()
 		if err != nil {
-			return nil, err
+			return nil,err
 		}
 
 		for _, playlistItem := range playlistResponse.Items {
-			playlistItems = append(playlistItems, playlistItem.Snippet.Title)
+			info := PlaylistInfo {
+				Title: playlistItem.Snippet.Title,
+				PlaylistItemID: playlistItem.Id,
+			}
+			playlistItems = append(playlistItems, info)
 		}
 
 		nextPageToken = playlistResponse.NextPageToken
@@ -38,4 +48,34 @@ func GetYoutubePlaylistItems(playlistID string, apiKey string) ([]string, error)
 	}
 
 	return playlistItems, nil
+}
+
+func DeleteSongByID(playlistID string, songID string, apiKey string) (error) {
+	ctx := context.Background()
+	youtubeService, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
+	if err != nil {
+		return err
+	}
+
+
+
+//tutaj musimy jakos wydobyc to id
+
+	if songID != "" {
+        err = youtubeService.PlaylistItems.Delete(songID).Do()
+        if err != nil {
+			fmt.Println("Error deleting playlist item")
+			return err
+            // if apiErr, ok := err.(*googleapi.Error); ok && apiErr.Code == http.StatusNotFound {
+            //     fmt.Println("Video not found in the playlist.")
+            // } else {
+            //     log.Fatalf("Error deleting playlist item: %v", err)
+            // }
+        } else {
+            fmt.Println("Video deleted successfully.")
+        }
+    } else {
+        fmt.Println("Video not found in the playlist.")
+    }
+	return nil
 }
